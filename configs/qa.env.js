505167bcf478aaa74ab43b4f001351c73484cc60
oshara <amanthi1995@gmail.com>
2025-12-28 21:01:22 +0530

    initial commit

---
+++ configs/qa.env.js
@@ -0,0 +1,15 @@
+export const QA_ENV ={
+    baseURL: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+
+    credentials:{
+        user:{
+            username: 'Admin',
+            password: 'admin123'    
+        },
+
+        invalidUser:{
+            username: 'user',
+            password : 'user123'
+        }
+    }
+}
