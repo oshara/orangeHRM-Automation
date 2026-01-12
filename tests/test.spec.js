13447f6c7aa54e7cb28a0cff4cacdf4ff4493488
oshara <amanthi1995@gmail.com>
2026-01-12 23:10:19 +0530

    Admin module - Jobs & Employment Status test spec

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,7 +5,12 @@ import { deleteUserTest } from './admin/user-management/delete_added_user';
 import { addJob } from './admin/jobs/job-titles/add_job';
 import { printoutJobTable } from './admin/jobs/job-titles/printout_job_table';
 import { basicJobTableFunc } from './admin/jobs/job-titles/basic_job_table_func';
-import { searchAddedJob } from './admin/jobs/job-titles/search_the_added_job';
+import { addNewPayGrade } from './admin/jobs/pay-grades/add_pay_grade';
+import { searchAddedPayGrade } from './admin/jobs/pay-grades/search_added_pay_grade';
+import { deletePayGrade } from './admin/jobs/pay-grades/delete_added_pay_grade';
+import { addEmploymentStatus } from './admin/jobs/employment-status/add_employment_status';
+import { searchAddedEmployStatus } from './admin/jobs/employment-status/search_added_employment_status';
+import { deleteEmploymentStatus } from './admin/jobs/employment-status/delete_employment_status';
 
 test.describe('Admin Module -Users section', () => {
 
@@ -22,5 +27,50 @@ test.describe('Admin Module -Users section', () => {
         await deleteUserTest(page);
     });
 
+
+});
+
+test.describe('Admin Module -Jobs section', () => {
+
+    test('Add Job Title', async ({ page }) => {
+        await addJob(page);
+    });
+
+    test('Print out the Job Table', async ({ page }) => {
+        await printoutJobTable(page);
+    })
+
+    test("Basic Job Table Functions", async ({ page }) => {
+        await basicJobTableFunc(page);
+    })
+
+    test("Search a Job Title that was added", async ({ page }) => {
+        await searchAddedJob(page);
+
+    });
+
+    test("Pay Grade - Add New Pay Grade", async ({ page }) => {
+        await addNewPayGrade(page);
+    });
+
+    test("Pay Grade - Search Added Pay Grade", async ({ page }) => {
+        await searchAddedPayGrade(page);
+    });
+
+    test('Pay Grade - Delete the added Pay Grade', async ({ page }) => {
+        await deletePayGrade(page);
+    })
+
+    test("Employment Status - Add New Employment Status", async ({ page }) => {
+        await addEmploymentStatus(page);
+    })
+
+    test("Employment Status - Search Added Employment Status", async ({ page }) => {
+        await searchAddedEmployStatus(page);
+    });
+
+    test.only('Employment Status - Delete the added Employment Status', async ({ page }) => {
+        await deleteEmploymentStatus(page);
+    });
 
 });
