0f8ac6ba673f320ca3cc10ed9812b1c7a3ef2fd5
oshara <amanthi1995@gmail.com>
2026-01-15 21:12:18 +0530

    update admin sub menu js

---
+++ pages/navigationMenus/admin/adminSubMenu.js
@@ -0,0 +1,61 @@
+export class AdminSubMenu {
+
+    constructor(page) { 
+        this.page= page;
+
+        this.jobMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]');
+        // Job Sub Menu Locators
+        this.jobTitleMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]/ul/li[1]/a');
+        this.payGradesMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]/ul/li[2]/a');
+        this.employmentStatusMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]/ul/li[3]');
+        this.jobCategoriesMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]/ul/li[4]/a');
+        this.workShiftsMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[2]/ul/li[5]/a');
+
+        // Organization Sub Menu Locators
+        this.generalInformationMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[3]/ul/li[1]/a');
+        this.locationsMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[3]/ul/li[2]/a');
+        this.structurMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[3]/ul/li[3]/a');
+
+        // Qualifications Sub Menu Locators
+        this.skillsMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[4]/ul/li[1]/a');
+        this.eductionMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[4]/ul/li[2]/a');
+        this.licensesMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[4]/ul/li[3]/a');
+        this.languagesMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[4]/ul/li[4]/a');
+        this.membershipsMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[4]/ul/li[5]/a');
+
+        // Nationalities Sub Menu Locators
+        this.nationalitiesMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[5]');
+
+        //Corporate Branding Sub Menu Locators
+        this.corporateBrandingMenu = page.locator('//div[@class="oxd-topbar-body"]/nav/ul/li[6]');
+
+        //Configuration Sub Menu Locators
+        
+
+        
+    }
+
+    async clickJobMenu(){
+        await this.jobMenu.click();
+    }
+
+    async clickJobTitleMenu(){
+        await this.clickJobMenu();
+        await this.jobTitleMenu.click();
+    }   
+
+    async clickPayGradeMenu(){
+        await this.clickJobMenu();
+        await this.payGradesMenu.click();
+    }
+
+    async clickEmploymentStatusMenu(){
+        await this.clickJobMenu();
+        await this.employmentStatusMenu.click();
+    }
+
+    async clickJobCategoriesMenu(){
+        await this.clickJobMenu();
+        await this.jobCategoriesMenu.click();
+    }
+}
