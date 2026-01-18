946b219f27ba102b2b9908b3bbb2545029ede3b6
oshara <amanthi1995@gmail.com>
2026-01-18 09:57:48 +0530

    search the added job category script

---
+++ tests/admin/jobs/job-categories/search_job_categories.js
@@ -0,0 +1,15 @@
+import { JobCategoriesPage } from "../../../../pages/admin/job/job-categories/jobCategoriesStatus";
+import { loginValid } from "../../../login/login_valid"
+import { navigateJobCategories } from "../../../menu-navigation/admin-menu-navigations/admin_menu_navigations";
+import { navigateToAdminMenu } from "../../../menu-navigation/navigation_menus";
+
+export const searchJobCategoriesFunc = async(page)=>{
+
+    await loginValid(page);
+    await navigateToAdminMenu(page);
+    await navigateJobCategories(page);
+
+    const JobCategoriesObj = new JobCategoriesPage(page);
+    await JobCategoriesObj.searchAddedJobCategory('Engineering');
+    await page.waitForTimeout(3000);
+}
