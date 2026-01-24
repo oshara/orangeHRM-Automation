2477bd437fca7661d94695855e7855b70687e4ee
oshara <amanthi1995@gmail.com>
2026-01-24 22:35:08 +0530

    delete employee status script

---
+++ tests/admin/jobs/employment-status/delete_employment_status.js
@@ -0,0 +1,16 @@
+import { EmploymentStatusPage } from "../../../../pages/admin/job/employment-status/employmentStatus";
+import { loginValid } from "../../../login/login_valid"
+import { navigateEmploymentStatus } from "../../../menu-navigation/admin-menu-navigations/admin_menu_navigations";
+import { navigateToAdminMenu } from "../../../menu-navigation/navigation_menus";
+
+export const deleteEmploymentStatus = async(page)=>{
+
+    await loginValid(page);
+    await navigateToAdminMenu(page);
+    await navigateEmploymentStatus(page);
+
+    const EmploymentStatusObj = new EmploymentStatusPage(page);
+    await EmploymentStatusObj.deleteAddedEmploymentStatus('Full-Time Permanent');
+    await page.waitForTimeout(3000);
+
+}
