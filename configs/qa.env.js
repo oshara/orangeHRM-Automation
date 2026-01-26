5de38ab83d59421ffc196b0a91e31826ce487355
oshara <amanthi1995@gmail.com>
2026-01-26 23:15:22 +0530

    add orgniazations- general infor values to configuration file

--- configs/qa.env.js
+++ configs/qa.env.js
@@ -11,5 +11,31 @@ export const QA_ENV ={
             username: 'user',
             password : 'user123'
         }
+    },
+
+
+
+    oragnizationInfor:{
+        generalInfor:{
+            organizationName :'Orange HRM New Edit',
+            employeeNumber: '220',
+            registrationNumber :'7006000',
+            taxIdNumber:'TX1002',
+            phoneNumber:'077918288929',
+            faxNumber:'TF9393820',
+            email:'generalInfor@gmail.com',
+            address1: '111/B Baker Street',
+            address2:'Near City Hospital',
+            cityName: 'Austin',
+            stateName: 'Texas',
+            countryName:'United States',
+            zipCode: '78701',
+            note:'This is general information edit note'
+
+
+
+
+        }
+
     }
 }
