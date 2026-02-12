ef35535062115e45f5dccc923b85c44bae890626
oshara <amanthi1995@gmail.com>
2026-02-12 22:48:02 +0530

    search the added skill

---
+++ tests/admin/qualifications/skills/search_skill.js
@@ -0,0 +1,14 @@
+import { SkillsPage } from "../../../../pages/admin/qualifications/skills/skills";
+import { loginValid } from "../../../login/login_valid"
+import { navigateToSkills } from "../../../menu-navigation/admin-menu-navigations/admin_menu_navigations";
+import { navigateToAdminMenu } from "../../../menu-navigation/navigation_menus";
+
+export const searchAddeSkillFunc = async(page)=>{
+
+    await loginValid(page);
+    await navigateToAdminMenu(page);
+    await navigateToSkills(page);
+
+    const SkillsObj = new SkillsPage(page);
+    await SkillsObj.searchAddedSkill("JIRA");
+}
