24269dffa3d4cf78c3f0bada021ff35050c99e93
oshara <amanthi1995@gmail.com>
2026-02-17 20:47:59 +0530

    added the search license script

--- pages/admin/qualifications/lisences/lisences.js
+++ pages/admin/qualifications/lisences/lisences.js
@@ -8,6 +8,8 @@ export class LicensesPage {
 
         this.successToastMessage = page.locator('//div[@class="oxd-toast-content oxd-toast-content--success"]/p[2]');
 
+        this.cellCount = page.locator('//div[@class="oxd-table-cell oxd-padding-cell"]');
+
 
 
     }
@@ -16,8 +18,19 @@ export class LicensesPage {
         await this.page.waitForTimeout(4000);
         await this.addNewLicenseButton.click();
         await this.addLicenseInputField.fill(licenseName);
+        await this.page.waitForTimeout(2000);
         await this.saveLicenseButton.click();
       
         await expect(this.successToastMessage).toHaveText('Successfully Saved');
 }
+
+
+   async searchAddedLicense(licenseName){
+    await this.page.waitForTimeout(4000);
+    const licenceCell = await this.page.locator('//div[@class="oxd-table-card"]/div/div/div',{
+        hasText:licenseName
+    })
+    await expect(licenceCell).toHaveText(licenseName);
+     console.log(licenceCell);  
+   }
 }
